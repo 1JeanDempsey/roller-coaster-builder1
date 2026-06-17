fdb84c66e297bf68dc4297a20bc532ffc960a3b5
bpcivball <seldeny076@gmail.com>
2026-06-16 21:12:07 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,7 @@ function MusicController() {
     dayMusic.volume = 0.5;
     setDaylightMusic(dayMusic);
     
-    const nightMusicAudio = new Audio(`${base}sounds/night1.mp3`);
+    const nightMusicAudio = new Audio(`${base}sounds/Night2.mp3`);
     nightMusicAudio.loop = true;
     nightMusicAudio.volume = 0.5;
     setNightMusic(nightMusicAudio);
